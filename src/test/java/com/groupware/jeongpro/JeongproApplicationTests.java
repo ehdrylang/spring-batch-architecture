225822ac555c1e0c3e7607d2395f1866bda870e5
정동교 <ehdrylang@naver.com>
2021-05-14 22:33:28 +0900

    first commit

---
+++ src/test/java/com/groupware/jeongpro/JeongproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupware.jeongpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
